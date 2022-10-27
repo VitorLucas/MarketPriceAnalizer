b687e3509fd2770ab9b17bcee64f52b13a054bc6
Vitor Santos <vsantos@alter-solutions.com>
2022-10-27 15:44:57 +0100

    feat: add configuration to store products

--- Helpers/Configuration.py
+++ Helpers/Configuration.py
@@ -3,3 +3,6 @@ import pathlib
 
 with open("configuration.json", "r") as configurations:
     config = json.load(configurations)
+
+with open("products.json", "r") as product_list:
+    product_list = json.load(product_list)
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from Integrations.IntegrationBuilder import integrations
 from Repository.DataBase import MarketDataBase
 from Helpers.LoggerHandler import logging
-from Helpers.Configuration import config
+from Helpers.Configuration import config, product_list
+
+items_to_find = product_list["name"]
 
 if __name__ == "__main__":
     logging.info(f'Enviroment.{config["environment"]}')
     logging.info('Start search for products.')
-    items_to_find = ['Banana', 'Uva']
 
-    #only to create database once
-    #MarketDataBase.create_table()
+    MarketDataBase.create_table()
 
-    for i in range(0, len(integrations)):
+    for i in range(0, len(product_list)):
         for j in range(0, len(items_to_find)):
             logging.info(f'Start search for products in {integrations[i].name} market')
             MarketDataBase.add_products(integrations[i].find(items_to_find[j]))
